c2146f5e23f0584e1b5932c8ba0b6b6aa3e7fabb
sultanowskii <dsr431@yandex.ru>
2020-04-21 21:41:16 +0300

    Fixed minor bug

--- bot/config.py
+++
@@ -1,8 +0,0 @@
-from random import randint
-
-ROOM_IMAGE_LIMIT = 6
-ROOM_USER_LIMIT = 6
-USER_ROOM_LIMIT = 6
-TOKEN = '1143604661:AAGFGL-165iA0wANFkvlS-jh2eAVxo784VY'  # change on server
-API_ADDRESS = 'http://localhost:5000'  # change on server
-
